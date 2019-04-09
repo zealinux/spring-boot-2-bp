9e7e34388edc33e19f49bf97f552027fd9a624ef
Mike King <zealinux@gmail.com>
2019-04-09 14:18:44 +0800

    feature: add json response

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,13 +1,18 @@
 package com.example.demo.controller;
 
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class HelloController {
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
     public String index() {
-        return "Hello World!";
+        String jsonStr = "{\"city\":\"chicago\",\"name\":\"jon doe\",\"age\":\"22\"}";
+        JSONObject jo = new JSONObject(jsonStr);
+        return jo.toString();
+
     }
 }
